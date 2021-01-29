02b7419fd07334f54b340ddb39e0010fa84f1fa6
AlimKuprianov <alimkosheev@gmail.com>
2021-01-29 16:08:21 +0300

    save project

---
+++ DayOfYear/ViewController.swift
@@ -0,0 +1,71 @@
+//
+//  ViewController.swift
+//  DayOfYear
+//
+//  Created by Алим Куприянов on 29.01.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var dateTF: UITextField!
+    @IBOutlet weak var monthTF: UITextField!
+    @IBOutlet weak var yearTF: UITextField!
+    
+    @IBOutlet weak var hereLabel: UILabel!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        hideKeyboardWhenTappedAround()
+    }
+    
+    @IBAction func DateButton(_ sender: UIButton) {
+        
+        guard let day = dateTF.text, let month = monthTF.text, let year = yearTF.text else { return }
+        
+        let calendar = Calendar.current
+        var dateComponents = DateComponents()
+        dateComponents.day = Int(day)
+        dateComponents.month = Int (month)
+        dateComponents.year = Int (year)
+        
+        
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "EEEE"
+        dateFormatter.locale = Locale.init(identifier: "ru_RU")
+        
+        guard   let date = calendar.date(from: dateComponents) else { return }
+        
+        let weekDay = dateFormatter.string(from:date)
+        let capitalizedWeekDay = weekDay.capitalized
+        
+        
+        
+        hereLabel.text = capitalizedWeekDay
+        
+        
+        
+        
+        
+       
+    }
+    
+    
+
+    
+}
+
+
+extension UIViewController {
+    func hideKeyboardWhenTappedAround() {
+        let tapGesture = UITapGestureRecognizer(target: self,
+                         action: #selector(hideKeyboard))
+        view.addGestureRecognizer(tapGesture)
+    }
+
+    @objc func hideKeyboard() {
+        view.endEditing(true)
+    }
+}
